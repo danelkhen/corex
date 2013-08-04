bc96afa5e2f3a209c431611cf9fbb16c46dab7a9
Dan-el <danelkhen@gmail.com>
2013-08-04 18:24:21 +0300

    FsPath

--- src/CorexDemo/Program.cs
+++ src/CorexDemo/Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Corex.IO;
 using Corex.IO.Tools;
 
 namespace CorexDemo
 {
     class Program
     {
+        static void foo()
+        {
+            FsPath xx = "C:\\";
+            var xxx = xx + "abc" + "def\\";
+
+        }
         static void Main(string[] args)
         {
+            foo();
             var args2 = new string[] { "/Name:Hello" };
             var tool = new ToolArgsInfo<Program>();
             var x = tool.Parse(args2);
--- src/corex/IO/FsPath.cs
+++ src/corex/IO/FsPath.cs
@@ -11,6 +11,7 @@ namespace Corex.IO
     {
         public FsPath(string path)
         {
+            Path.IsPathRooted(path);
             _Value = path;
         }
         public static implicit operator FsPath(string value)
@@ -21,16 +22,16 @@ namespace Corex.IO
         {
             return path._Value;
         }
-        string _Value;
-        public static FsPath Create(string path)
+        public static FsPath operator +(FsPath x, FsPath y)
         {
-            return new FsPath(path);
+            return x.Child(y);
         }
+        string _Value;
         public FsPath Parent
         {
             get
             {
-                return Create(Path.GetDirectoryName(_Value));
+                return new FsPath(Path.GetDirectoryName(_Value));
             }
         }
         public FsPath this[string name]
@@ -40,31 +41,47 @@ namespace Corex.IO
                 return Child(name);
             }
         }
+        public FileInfo ToFileInfo()
+        {
+            return new FileInfo(_Value);
+        }
+        public DirectoryInfo ToDirectoryInfo()
+        {
+            return new DirectoryInfo(_Value);
+        }
         public FsPath Child(string name)
         {
-            return Create(Path.Combine(_Value, name));
+            return CombineWith(name);
+        }
+        public FsPath CombineWith(string name)
+        {
+            return new FsPath(Path.Combine(_Value, name));
         }
         public FsPath Sibling(string name)
         {
             return Parent.Child(name);
         }
-        public FsPath FileNameWithoutExtension
+        public FsPath NameWithoutExtension
         {
             get
             {
-                return Create(Path.GetFileNameWithoutExtension(_Value));
+                return new FsPath(Path.GetFileNameWithoutExtension(_Value));
             }
         }
-        public FsPath FileName
+        public FsPath Name
         {
             get
             {
-                return Create(Path.GetFileName(_Value));
+                return new FsPath(Path.GetFileName(_Value));
             }
         }
         public FsPath ChangeExtension(string ext)
         {
-            return Create(Path.ChangeExtension(_Value, ext));
+            return new FsPath(Path.ChangeExtension(_Value, ext));
+        }
+        public override string ToString()
+        {
+            return _Value;
         }
     }
 }
