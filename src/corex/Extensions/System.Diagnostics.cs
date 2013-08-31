0ee1a992aca2bd6147f53c4d2846eecc6d9d2b38
danelkhen <danelkhen@gmail.com>
2013-08-31 14:38:59 +0300

    ProcessHelper

---
+++ src/corex/Extensions/System.Diagnostics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Diagnostics
+{
+    public static class DiagnosticsExtensions
+    {
+        public static string MainModuleFilename(this Process p)
+        {
+            return p.MainModule.FileName;
+        }
+    }
+}
---
+++ src/corex/Helpers/ProcessHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corex.Helpers
+{
+    public static class ProcessHelper
+    {
+        public static Process Current
+        {
+            get
+            {
+                return Process.GetCurrentProcess();
+            }
+        }
+
+        public static FileInfo CurrentProcessFile { get { return Current.MainModule.FileName.ToFileInfo(); } }
+    }
+}
