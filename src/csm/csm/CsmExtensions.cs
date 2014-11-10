213677446f0364fe1a0c748bc11e5ac94197c192
unknown <dkhen@AMS9-2242.booking.pcln.com>
2014-11-10 13:51:01 +0100

    add notepad++ theme in vs2013 dark style - based on SeanCline

--- src/csm/csm/CsmExtensions.cs
+++ src/csm/csm/CsmExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections;
 
 namespace Csm
 {
@@ -16,6 +17,34 @@ namespace Csm
         public static void ExecuteAsCsmScript(this string script)
         {
             CsmRuntime.VerifyInit().Run(new CsmScript { Code = script });
+        }
+
+
+        public static IEnumerable<T> ToEnumerable<T>(this IEnumerable list)
+        {
+            return new TypedEnumerable<T>(list);
+        }
+
+        class TypedEnumerable<T> : IEnumerable<T>
+        {
+            private IEnumerable Source;
+            public TypedEnumerable(IEnumerable source)
+            {
+                Source = source;
+            }
+            
+            public IEnumerator<T> GetEnumerator()
+            {
+                foreach(T t in Source)
+                {
+                    yield return t;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
--- src/csm/csm/Program.cs
+++ src/csm/csm/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Csm
